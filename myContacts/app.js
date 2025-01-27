cda5fa3799375d36372670fcca880ca52523838b
jooho-le <wngh9696@naver.com>
2025-01-27 12:02:33 +0900

    1/26

--- myContacts/app.js
+++ myContacts/app.js
@@ -7,6 +7,32 @@ app.get("/", (req, res) => {
     res.send("hello node");
 });
 
+
+// 모든 연락처 가져오기
+app.get("/contacts", (req, res) => {
+    res.status(200).send("Contacts Page");
+});
+
+// 새 연락처 추가하기
+app.post("/contacts", (req, res) => {
+    res.status(200).send("Create Contact");
+});
+
+// 연락처 상세보기
+app.get("/contacts/:id", (req, res) => {
+    res.status(200).send(`View Contact for ID : ${req.params.id}`);
+});
+
+// 연락처 수정하기
+app.put("/contacts/:id", (req, res) => {
+    res.status(200).send(`Update Contact for ID : ${req.params.id}`);
+});
+
+// 연락처 삭제하기
+app.delete("/contacts/:id", (req, res) => {
+    res.status(200).send(`Delete Contact for ID : ${req.params.id}`);
+});
+
 app.listen(port, () => {
     console.log(`${port}번 포트에서 서버 실행 중`);
 });
